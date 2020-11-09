3f71a33c7d768758c3e426027277914c91b32108
Aman Zadran <amanzadran9@gmail.com>
2020-11-09 14:07:58 +0100

    feat: effects added, and mulitple selection of subreddits possible

--- app.js
+++ app.js
@@ -1,37 +1,60 @@
 $(document).ready(function () {
-  const subReddit = "cats";
+  const renderPosts = (subReddit) => {
+    if (subReddit === undefined) {
+      subReddit = "memes";
+    }
 
-  $.ajax(`https://www.reddit.com/r/${subReddit}/top.json?limit=10`, {
-    success: function (responseData) {
-      const { data } = responseData;
-      const resultList = data.children;
+    $.ajax(`https://www.reddit.com/r/${subReddit}/top.json?limit=10`, {
+      success: function (responseData) {
+        const { data } = responseData;
+        const resultList = data.children;
 
-      $.each(resultList, function (index, element) {
-        const res = element.data;
+        $.each(resultList, function (index, element) {
+          const res = element.data;
 
-        const thumbnail = res.preview.images[0].source.url || res.thumbnail;
+          // Better than .url, since that will sometimes only link to an image(in case of memes)
+          let urlToPost = "http://reddit.com" + res.permalink;
 
-        $(".row").append(
-          $(`
-        
-            <a href="${res.url}" class="col">  
+          let thumbnail = res.thumbnail;
 
-            <img src="${thumbnail}" alt="${res.title}">
-            
-            <div class="col-content"> 
-            <h3>${res.title}</h3>
+          if (
+            res.preview &&
+            res.preview.images[0] &&
+            res.preview.images[0].source
+          ) {
+            thumbnail = res.preview.images[0].source.url;
+          }
 
-            </div>
+          let col = $(`   <a href="${urlToPost}" class="col">  
+      
+          <img src="${thumbnail}" alt="${res.title}">
+          
+          <div class="col-content"> 
+          <h3>${res.title}</h3>
 
-            </a>
+          </div>
 
-        
-        `)
-        );
-      });
-    },
-    error: function () {
-      alert("Something didn't work!");
-    },
+          </a>`)
+            .hide()
+            .fadeIn(800);
+
+          $(".row").append(col);
+        });
+      },
+      error: function () {
+        alert("Something didn't work!");
+      },
+    });
+  };
+
+  renderPosts();
+
+  $("button").click(function () {
+    let subRedd = $(this).text();
+    subRedd = subRedd.toLowerCase();
+
+    $(".row").empty();
+
+    renderPosts(subRedd);
   });
 });
